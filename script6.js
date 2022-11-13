861fde243bf629a1590de49c04396f302aab039f
VoronWorks <113391544+VoronWorks@users.noreply.github.com>
2022-11-13 14:22:01 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -10,7 +10,6 @@ function updatePrice() {
   }
   let im = document.getElementById("image");
   im.src="work6/image"+select.value+".png";
-   console.log(im.src);
   let radioDiv = document.getElementById("radios");
   radioDiv.style.display = (select.value == "2" ? "block" : "none");
   
@@ -21,7 +20,7 @@ function updatePrice() {
       let optionPrice = prices.myradios[radio.value];
       if (optionPrice !== undefined) {
         price += optionPrice;
-        im.src="image"+select.value+radio.value+".png";
+        im.src="work6/image"+select.value+radio.value+".png";
       }
       if(select.value!=2)
       radio.checked=false;
@@ -40,7 +39,7 @@ function updatePrice() {
       let propPrice = prices.prodProperties[checkbox.name];
       if (propPrice !== undefined) {
         price += propPrice;
-        im.src="image"+select.value+checkbox.name+".png";
+        im.src="work6/image"+select.value+checkbox.name+".png";
       }
       if(select.value!=3)
       checkbox.checked=false;
