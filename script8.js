aacfd939275a752f87e928d2a6a00703f64984fc
VoronWorks <113391544+VoronWorks@users.noreply.github.com>
2022-12-11 20:04:56 +0300

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,14 +1,19 @@
 const nextURL = 'https://voronworks.github.io/index8.html';
 const nextTitle = 'My new page title';
 const nextState = { additionalInformation: 'Updated the URL with JS' };  
+ const form = document.getElementById('form');
 window.addEventListener('DOMContentLoaded', function (event) {
 let b = document.getElementById("my-button");
+form.style.display = 'none';
 b.addEventListener("click", click1);
+
+
+
 });
 function click1() {
 window.history.pushState(nextState, nextTitle, nextURL);
-  window.open('https://voronworks.github.io/index8.html')
-  if (form.style.display === 'none') {
+
+if (window.location.href == 'https://voronworks.github.io/index8.html') {
 
   form.style.display = 'block';
 } else {
