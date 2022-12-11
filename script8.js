0ce5143065aa14396057bd72866d466de2601f66
VoronWorks <113391544+VoronWorks@users.noreply.github.com>
2022-12-11 19:35:01 +0300

    Update script8.js

--- script8.js
+++ script8.js
@@ -2,8 +2,8 @@ const nextURL = 'https://voronworks.github.io/index8.html';
 const nextTitle = 'My new page title';
 const nextState = { additionalInformation: 'Updated the URL with JS' };  
 window.addEventListener('DOMContentLoaded', function (event) {
-b.addEventListener("click", click1);
 let b = document.getElementById("my-button");
+b.addEventListener("click", click1);
 });
 function click1() {
 window.history.pushState(nextState, nextTitle, nextURL);
