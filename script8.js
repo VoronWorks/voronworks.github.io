e1ab04d26f21ae4bb77a166f1216e5d139cb3eb5
VoronWorks <113391544+VoronWorks@users.noreply.github.com>
2022-12-19 22:46:50 +0300

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,31 +1,77 @@
-
 window.addEventListener('DOMContentLoaded', function (event) {
-var closedModalHashStateId = "";
-      var openModalHashStateId = "#form";
-       window.location.hash = closedModalHashStateId;
-      $('#myModal').on('show.bs.modal', function(e) {
-        window.location.hash = openModalHashStateId;
-      });  
+var state = null;var t=1;
+      var btn2 = document.getElementById('my-button');
+var url = '#form';
+btn2.addEventListener('click', function Click2(event) {
+    if(state==null)
+    {history.pushState("1", '', url)
+state=history.state; t=2;}
+    else history.replaceState("2", '', url)
+});
+$(window).on('popstate', function (e) {
+    if(t==2)
+    {history.back();
+        $('#myModal').modal('hide');t=1;
+    state=null;}
+        else {history.forward(); t=2;}
+    
+});
 
-     
-          $(window).on('hashchange', function (event) {
-        if(window.location.hash == closedModalHashStateId) {
-            $('#myModal').modal('hide');
-        }
-    });
-    fetch('https://api.slapform.com/bB3DyW5Odz', {
-      method: "POST",
-  body: JSON.stringify({
-    name: 'Jon Snow',
-    email: 'email.com',
-    message: 'Hello World!',
-    })
-    })
-    .then(function (response) {
-    console.log('Success', response)
-    })
-    .catch(function (e) {
-    console.error('Fail', e)
-    })
+  var nameInput = document.getElementById('field-name');
+  var emailInput = document.getElementById('field-email');
+  var msgInput = document.getElementById('field-msg');
+ 
+         
+       window.onbeforeunload=function(){
+localStorage.setItem("name",$('#field-name').val());
+localStorage.setItem("email",$('#field-email').val());
+localStorage.setItem("msg",$('#field-msg').val());
+       }
+       window.onload = function(){
+        var name= localStorage.getItem("name");
+        if(name!=null) $('#field-name').val(name);
+        var email= localStorage.getItem("email");
+        if(email!=null) $('#field-email').val(email);
+        var msg= localStorage.getItem("msg");
+        if(msg!=null) $('#field-msg').val(msg);
+       }
+        var btn = document.getElementById('sub-button');
+
+btn.addEventListener('click', function Click1(event) {
+
+  //event.preventDefault();
+
+  
+ 
+$(function(){
+      $(".ajaxForm").submit(function(e){
+          e.preventDefault();
+          var href = $(this).attr("action");
+          $.ajax({
+              type: "POST",
+              dataType: "json",
+              url: href,
+              data: $(this).serialize(),
+              success: function(response){
+                  if(response.status == "success"){
+                      {alert("We received your submission, thank you!");
+                      nameInput.value = '';
+                      emailInput.value = '';
+                       msgInput.value = '';
+                    }
+                  }else{
+                      alert("An error occured: " + response.message);
+                      nameInput.value = '';
+                      emailInput.value = '';
+                       msgInput.value = '';
+                  }
+              }
+          });
+      });
+  });
+  
+   
+});
 
     });
+    
